5b8fef5e82a833b0b33a90fd310b3f385fdc5120
Nounsidev <136212891+Nounsidev@users.noreply.github.com>
2025-09-26 21:47:57 +0700

    Main ESP32 and ARDUINO files

---
+++ Arduino.ino
@@ -0,0 +1,339 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <LoRa.h>
+#include <BH1750.h>
+
+//Pin LoRa RA-02
+#define LORA_NSS 10
+#define LORA_RST 9
+#define LORA_DIO0 2
+
+#define LED 7
+
+const byte masterAddr = 0x01;
+const byte loraAddr[] = {0x02, 0x03};
+const int thisLoraAddr = 0;
+
+unsigned long previousMillisRestartLORA = 0;
+const long intervalRestartLORA = 5000;
+
+String incomingMessage = "";
+String message = "";             
+char separator = ',';
+
+bool isLedOn = false;
+bool lastLedState = false;
+bool isSensorOn = false;
+
+String Slux;  
+BH1750 lightMeter;
+
+const unsigned long LED_ON_THRESHOLD = 400.0;  // Bật đèn nếu ánh sáng dưới 100 lux
+const unsigned long LED_OFF_THRESHOLD = 1000.0; // tắt đèn nếu ánh sáng trên 200 lux
+
+// Biến cho Moving Average Filter
+const int WINDOW_SIZE = 10;
+float luxReadings[WINDOW_SIZE];
+int readingIndex = 0;
+
+unsigned long sensorDelayTimer = 0;
+int sensorDelayTime = 10000;
+
+// Biến để quản lý non-blocking delay cho việc đọc cảm biến
+unsigned long previousMillis = 0;
+const long interval = 10; // Đọc cảm biến mỗi 0.6 giây
+
+// Biến lưu giá trị lux trung bình, có thể truy cập từ mọi nơi
+unsigned long averageLux = 0;
+bool isProcessingDone = false; // Biến cờ để báo hiệu một chu kỳ đã hoàn thành
+
+double r1 = 1000;
+double r2 = 1000;
+double vcc = 8.21;
+float batteryPercentage = 0.018;
+float minBattery = 6.6;
+#define batteryPin A0
+int batteryLevel = 50;
+int batteryReadCount = 10;
+
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, isLedOn ? HIGH : LOW);
+  Wire.begin();       // Khởi tạo giao tiếp I2C
+  
+  if (lightMeter.begin()) 
+  {
+    Serial.println("Cảm biến BH1750 đã sẵn sàng.");
+  } 
+  else 
+  {
+    Serial.println("Lỗi khởi động cảm biến BH1750!");
+  }
+  while (!Serial);
+  ResetLora();
+  sensorDelayTimer = millis();
+
+}
+
+void loop() 
+{
+  delay(50);
+  
+  OnReceive(LoRa.parsePacket());
+  LoraResetHandler();
+  if(isSensorOn)
+  {
+    ReadLightLevel();
+  }
+}
+
+//******************************LED HANDLER**************************
+void ProcessingLedData()
+{
+  //Serial.println("Arduino Message Receive " + String(isLedOn));
+  digitalWrite(LED, isLedOn ? HIGH : LOW);
+}
+
+//Code này dùng để tách khi gửi có nhiều dữ liệu cần tách thành chuỗi, Đối với ESP32 nhận bool isLedOn thì không cần
+//Đối với Arduino nhận 2 bool isSensorOn, isLedOn thì cần tách một string thành những substring
+//Data sẽ là string dữ liệu (parameter: String data) cách nhau bởi dấu " , "
+//Hàm sẽ tìm index đầu cuối của string dữ liệu với vị trí xếp trong chuỗi cho trước (paramater: int index)
+//Sau đó sẽ dùng hàm substring để lấy string dữ liệu cần dùng ra
+String GetValue(String data, int index) 
+{
+  int found = 0;
+  int strIndex[] = { 0, -1 };
+  int maxIndex = data.length() - 1;
+  
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+//*****************************END LED HANDLER**********************
+
+
+
+//******************************LORA HANDLER*****************************
+//Cú pháp gửi dữ liệu /Địa chỉ nhận/Địa chỉ gửi/Data/
+void SendLora(String Outgoing, byte Destination) {
+  LoRa.beginPacket();             //--> start packet
+  LoRa.write(Destination);        //--> add destination address
+  LoRa.write(loraAddr[thisLoraAddr]);        //--> add sender address
+  LoRa.write(Outgoing.length());  //--> add payload length
+  LoRa.print(Outgoing);           //--> add payload
+  LoRa.endPacket();               //--> finish packet and send it
+}
+
+void OnReceive(int packetSize) {
+  if (packetSize == 0) return;          // if there's no packet, return
+
+  //Serial.println("Receive LORA");
+  //read packet header bytes:
+  int recipient = LoRa.read();        //--> recipient address
+  byte sender = LoRa.read();          //--> sender address
+  byte incomingLength = LoRa.read();  //--> incoming msg length
+
+  //Condition that is executed if message is not from Master.
+  if (sender != masterAddr) {
+    //Serial.println(sender);
+    Serial.println("Not from Master, Ignore");
+  	Serial.println(sender);
+    // Resets the value of the Count_to_Rst_LORA variable.
+    return;
+  }
+
+  // Clears Incoming variable data.
+  incomingMessage = "";
+
+  //Get all incoming data.
+  while (LoRa.available()) {
+    incomingMessage += (char)LoRa.read();
+  }
+
+
+  //Check length for error.
+  if (incomingLength != incomingMessage.length()) {
+    Serial.println();
+    Serial.println("er"); //--> "er" = error: message length does not match length.
+    //Serial.println("error: message length does not match length");
+    return;
+  }
+
+  //Checks whether the incoming data or message for this device.
+  if (recipient != loraAddr[thisLoraAddr]) 
+  {
+    Serial.println("This message is not for me.");
+    return; 
+  } 
+  else 
+  {
+    // if message is for this device, or broadcast, print details:
+    Serial.println();
+    //Serial.println("Rc from: 0x" + String(sender, HEX));
+    //Serial.println("Message: " + incomingMessage);
+
+    // Calls the Processing_incoming_data() subroutine.
+    ProcessingIncomingData(sender);
+  }
+}
+
+void ProcessingIncomingData(byte sender) 
+{  
+  //Cú pháp tách dữ liệu lora isSensorOn,isLedOn
+  isSensorOn = GetValue(incomingMessage, 1).toInt();
+  
+
+  Serial.println("******************************");
+  Serial.println("Rc from : 0x" + String(sender));
+  Serial.println("Message: " + incomingMessage);
+
+  if(!isSensorOn)
+  {
+    isLedOn = GetValue(incomingMessage, 0).toInt();
+    ProcessingLedData();
+    LoraMessage();
+  }
+  else 
+  {
+    LoraMessage();
+  }
+}
+
+void LoraMessage()
+{
+  ReadBattery();
+  String ledData = isLedOn ? "1" : "0"; 
+  message = String(isLedOn) + ',' + String(batteryLevel);
+  Serial.println("Send Message: " + message);
+  SendLora(message, masterAddr);
+}
+
+
+//Lora hoạt động thời gian dài có lúc bị đơ
+//Workaround -> Reset lora theo định kỳ
+void LoraResetHandler()
+{
+  unsigned long currentMillisRestartLORA = millis();  
+  if (currentMillisRestartLORA - previousMillisRestartLORA >= intervalRestartLORA) 
+  {
+    previousMillisRestartLORA = currentMillisRestartLORA;
+    LoRa.end();
+    ResetLora();
+  }
+}
+
+
+void ResetLora() 
+{
+   pinMode(LORA_NSS, OUTPUT);
+  LoRa.setPins(LORA_NSS, LORA_RST, LORA_DIO0);
+
+ //Serial.println();
+  Serial.println("Restart LoRa...");
+  //Serial.println("Start LoRa init...");
+  while (!LoRa.begin(433E6)) 
+  {
+    Serial.println("Starting LoRa failed!");
+    delay(100);
+  }
+  //Serial.println("LoRa init succeeded.");
+}
+
+void ReadLightLevel()
+{
+  unsigned long currentMillis, currentLux, sum;
+  // Chỉ thực hiện khi một chu kỳ xử lý mới chưa bắt đầu
+  if (!isProcessingDone) 
+  {
+    currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) 
+    {
+
+      previousMillis = currentMillis;
+
+      currentLux = lightMeter.readLightLevel();
+
+      Serial.print(readingIndex + 1);
+      Serial.print(": ");
+      Serial.print(currentLux);
+      Serial.println(" lux");
+        
+      luxReadings[readingIndex] = currentLux;
+      readingIndex++;
+
+      if (readingIndex >= WINDOW_SIZE) 
+      {
+        isProcessingDone = true; // Báo hiệu đã đủ dữ liệu
+            
+        // Tính giá trị trung bình sau khi mảng đã đầy
+        sum = 0;
+        for (int i = 0; i < WINDOW_SIZE; i++) 
+        {
+          sum += luxReadings[i];
+        }
+        averageLux = sum / WINDOW_SIZE;
+        controlLed(averageLux);
+        Serial.print("Giá trị Lux trung bình ");
+        Serial.println(averageLux);
+      }
+    }
+  } 
+  else 
+  {
+    if(millis() - sensorDelayTimer < sensorDelayTime) return;
+
+    sensorDelayTimer = millis();
+    isProcessingDone = false;
+    readingIndex = 0;
+    Serial.println("\n--- Bắt đầu chu kỳ thu thập dữ liệu mới ---\n");
+  }
+}
+
+void controlLed(unsigned long lux) 
+{
+    if (lux > LED_OFF_THRESHOLD) 
+    {
+      digitalWrite(LED, LOW);
+      isLedOn = false;
+      Serial.println("Tắt đèn.");
+    }
+    else if (lux < LED_ON_THRESHOLD) 
+    {
+      digitalWrite(LED, HIGH);
+      isLedOn = true;
+      Serial.println("Bật đèn.");
+    }
+    lastLedState = isLedOn;
+}
+
+//***************************END LORA HANDLER****************************
+
+//**************************** BATTERY HANDLER*****************************
+void ReadBattery()
+{
+  int sumBattery = 0;
+  for(int i =0; i < batteryReadCount; i++)
+  {
+    float readValue = analogRead(batteryPin);
+
+    double vOut = readValue * (double)0.0048875855;
+    double batteryVoltage = vOut / (double)(r2 /(r1 + r2));
+    //Serial.println("Vout " + String(vOut));
+  
+    sumBattery += ((batteryVoltage-minBattery) / batteryPercentage);
+  }
+
+  //Serial.print("Battery Level: " + String(batteryLevel));
+  batteryLevel = sumBattery / batteryReadCount;
+  Serial.println("Battery: " + batteryLevel);
+  
+}
+
---
+++ ESP32.ino
@@ -0,0 +1,399 @@
+#include <WiFi.h>
+#include <Wire.h>
+#include <FirebaseESP32.h>
+#include <LoRa.h>
+#include <SPI.h>
+#include <TimeLib.h>
+
+//Change WIFI and Password before compiling
+//#define WIFI_SSID "Anh Lan"
+//#define WIFI_PASSWORD "0348350899"
+
+#define WIFI_SSID "NounsiDev Network"
+#define WIFI_PASSWORD "noun0308"
+
+
+//#define FIREBASE_HOST "htps://smartlight-529a6-default-rtdb.firebaseio.com/"
+#define FIREBASE_HOST "https://smartlight-a512f-default-rtdb.firebaseio.com/"
+//"https://esp32-test-486b7-default-rtdb.firebaseio.com/"
+//#define FIREBASE_AUTH "TQB6PIhaqySmgNTRTDcbvNmlzsaHmCcrfvcWTVkC"
+#define FIREBASE_AUTH "9jeu6XMl9u3ZYJBFQDhFzsac8DdIQYtVmCHWhJpn"
+
+//"k8VKBQwPQKp4iNzhm1XggTu7MHvhuduwUxuwLsj2"
+
+//De
+#define ss 5
+#define rst 14
+#define dio0 2
+
+FirebaseData firebaseData;
+FirebaseAuth auth;
+FirebaseConfig config;
+String dataSize = "tiny";
+ulong lastDataReadMillis = 0;
+
+//setup read rate
+const int readDataLimit = 1000;
+
+// On - OFF
+const String ledStateLocation[] = { "/devices/4xxArxOlk1MSKPQ0eMGT/power/isLedOn", "/devices/zzeDW8HAAV6jCZydlLmA/power/isLedOn" };
+const String lightSensorLocation[] = { "/devices/4xxArxOlk1MSKPQ0eMGT/power/isLightSensorOn", "/devices/zzeDW8HAAV6jCZydlLmA/power/isLightSensorOn" };
+const String updateTimeStampLocation[] = { "/devices/4xxArxOlk1MSKPQ0eMGT/power/UpdateTime", "/devices/zzeDW8HAAV6jCZydlLmA/power/UpdateTime" };
+const String timeStampLocation = "Test/TimeStamp";
+const String batteryLocations[] = { "/devices/4xxArxOlk1MSKPQ0eMGT/power/batteryLevel", "/devices/zzeDW8HAAV6jCZydlLmA/power/batteryLevel" };
+
+bool isLedOn[] = { false, false };
+bool lastLedState[] = { false, false };
+
+bool isLightSensorOn[] = { false, false };
+bool lastLightSensorState[] = { false, false };
+
+const byte masterAddr = 0x01;
+const byte loraAddr[] = {0x02, 0x03};
+byte slaveAddr;
+
+int loraSendIndex = 0;
+
+int resetLoraCount = 0;
+unsigned long previousMillisRestartLORA = 0;
+const long intervalRestartLORA = 5000;
+
+String incomingMessage = "";
+String message = "";
+char separator = ',';
+int batteryLevels[] = {0, 0};
+int lastBatteryLevels[] = {0, 0};
+bool isTimeStampInit = false;
+double gmt_7 = 7 * 3600;
+
+int loraSendRate = 2000;
+unsigned long loraSendCounter = 0;
+
+
+  void setup() 
+{
+  Serial.begin(9600);
+  while (!Serial);
+
+  WifiInit();
+  FirebaseInit();
+  //DataInit();
+
+  ResetLora();
+}
+
+void loop() {
+  delay(50);
+
+  LedHandler();
+  OnReceive(LoRa.parsePacket());
+
+  LoraResetHandler();
+
+  //Only read data when time elaspe greater than readDataLimit || first boots up
+  if ((millis() - lastDataReadMillis < readDataLimit && lastDataReadMillis != 0) || !Firebase.ready()) return;
+  ReadData(loraSendIndex);
+  lastDataReadMillis = millis();
+}
+
+
+//*****************************INIT FUNCTION********************************
+void WifiInit() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wifi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+
+  Serial.println("******************");
+  Serial.print("Connected With IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("******************");
+}
+
+void FirebaseInit() {
+  //Setup
+  config.host = FIREBASE_HOST;
+  config.signer.tokens.legacy_token = FIREBASE_AUTH;
+
+  //Connecting
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
+  //Set database read timeout to 1min (max 15 mins)
+  Firebase.setReadTimeout(firebaseData, 1000 * 60);
+  //Write size time out tiny-1s, small-10s, medium-30s, large-60s
+  Firebase.setwriteSizeLimit(firebaseData, dataSize);
+}
+/*
+void DataInit() {
+  Firebase.setBool(firebaseData, ledStateLocation[0], false);
+  Firebase.setBool(firebaseData, ledStateLocation[1], false);
+
+  Firebase.setBool(firebaseData, sensorLocation[0], false);
+  Firebase.setBool(firebaseData, sensorLocation[1], false);
+
+  Firebase.setString(firebaseData, updateTimeStampLocation[0], GetTimeStamp());
+  Firebase.setString(firebaseData, updateTimeStampLocation[1], GetTimeStamp());
+}*/
+//**************************END INIT************************************
+
+//*************************TIME STAMP***********************************
+String GetTimeStamp() 
+{
+  String result = "";
+  Serial.printf("Set timestamp... %s\n", Firebase.setTimestamp(firebaseData, timeStampLocation) ? "ok" : firebaseData.errorReason().c_str());
+
+  if (firebaseData.httpCode() == FIREBASE_ERROR_HTTP_CODE_OK) 
+  {
+    // Timestamp saved in millisecond, get its seconds from int value
+    Serial.print("TIMESTAMP (Seconds): ");
+    Serial.println(firebaseData.to<int>());
+  }
+
+  Serial.printf("Get timestamp... %s\n", Firebase.getDouble(firebaseData, timeStampLocation) ? "ok" : firebaseData.errorReason().c_str());
+  if (firebaseData.httpCode() == FIREBASE_ERROR_HTTP_CODE_OK) 
+  {
+    double timeStamp = firebaseData.to<uint64_t>();
+
+    setTime(timeStamp / 1000 + gmt_7);
+    int mil = (int)timeStamp % 1000;
+    result = String(year()) + "-" + String(month()) + "-" + String(day()) + "T" + String(hour()) + ":" + String(minute()) + ":" + String(second()) + "." + String(mil) + "Z";
+  }
+  return result;
+}
+//**********************END TIME STAMP**********************************
+
+
+//**************************READ WRITE FIREBASE*************************
+void LedHandler() {
+
+  if(millis() - loraSendCounter < loraSendRate) return;
+  loraSendCounter = millis();
+  String sendMessage =  String(isLedOn[loraSendIndex]) +  "," + String(isLightSensorOn[loraSendIndex]);
+  SendLora(loraSendIndex, sendMessage);
+    //Serial.println("Send LoRa: " + String (loraSendIndex));
+  loraSendIndex++;
+  if (loraSendIndex > 1) loraSendIndex = 0;
+
+}
+
+void ReadData(int id) 
+{
+  if (!Firebase.getBool(firebaseData, ledStateLocation[id])) 
+  {
+    Serial.print("Firebase Error: ");
+    Serial.println(firebaseData.errorReason());
+
+  } 
+  else 
+  {
+    isLedOn[id] = firebaseData.boolData();
+    if (lastLedState[id] != isLedOn[id]) 
+    {
+      lastLedState[id] = isLedOn[id];
+      //Test
+      Serial.println("                                                                  ************");
+      Serial.print("                                                            ");
+      Serial.println("Trang thái đèn" + String(id) + ": " + String(lastLedState[id]));
+    }
+  }
+
+  if (!Firebase.getBool(firebaseData, lightSensorLocation[id])) 
+  {
+    Serial.print("Firebase Error: ");
+    Serial.println(firebaseData.errorReason());
+  } 
+  else 
+  {
+    isLightSensorOn[id] = firebaseData.boolData();
+    if (lastLightSensorState[id] != isLightSensorOn[id]) 
+    {
+      lastLightSensorState[id] = isLightSensorOn[id];
+      Serial.println("                                                                  ************");
+      Serial.print("                                              ");
+      Serial.println("Trang thai cam bien anh sang" + String(id) + ": " + String(isLightSensorOn[id]));
+    }
+  }
+}
+  //Only write to server when using sensor
+  void WriteFirebaseData(int index, bool isLedStateChange, bool isBatteryStateChanged) {
+    bool isAnyDataChanged = false;
+    
+    if(isLedStateChange) 
+    {
+      Firebase.setBool(firebaseData, ledStateLocation[index], isLedOn[index]);
+      isAnyDataChanged = true;
+    }
+
+    if(isBatteryStateChanged)
+    {
+      Firebase.setInt(firebaseData, batteryLocations[index], batteryLevels[index]);
+      isAnyDataChanged = true;
+    }
+
+    if(!isAnyDataChanged) return;
+    Firebase.setString(firebaseData, updateTimeStampLocation[index], GetTimeStamp());
+  }
+  //**************************END READ WRITE FIREBASE*************************
+
+
+  //******************************LORA HANDLER********************************
+  //Cú pháp gửi dữ liệu /Địa chỉ nhận/Địa chỉ gửi/Data/
+  void SendLora(int id, String Outgoing) {
+    //Serial.println("Send LORA");
+    LoRa.beginPacket();             //--> start packet
+    LoRa.write(loraAddr[id]);       //--> add destination address
+    LoRa.write(masterAddr);         //--> add sender address
+    LoRa.write(Outgoing.length());  //--> add payload length
+    LoRa.print(Outgoing);           //--> add payload
+    LoRa.endPacket();               //--> finish packet and send it
+  }
+
+  void OnReceive(int packetSize) {
+    if (packetSize == 0) return;  // if there's no packet, return
+
+    //Read packet header bytes:
+    int recipient = LoRa.read();        //--> recipient address
+    byte sender = LoRa.read();          //--> sender address
+    byte incomingLength = LoRa.read();  //--> incoming msg length
+
+    // Get the address of the senders or slaves.
+    slaveAddr = sender;
+
+    // Clears Incoming variable data.
+    incomingMessage = "";
+
+    // Get all incoming data / message.
+    while (LoRa.available()) {
+      incomingMessage += (char)LoRa.read();
+    }
+
+    // Resets the value of the Count_to_Rst_LORA variable if a message is received.
+    resetLoraCount = 0;
+
+    //Check length for error.
+    if (incomingLength != incomingMessage.length()) {
+      Serial.println("error: message length does not match length");
+      return;
+    }
+
+    //Checks whether the incoming data or message for the master or not
+    if (recipient != masterAddr) {
+      Serial.println("This message is not for me.");
+      return;
+    }
+
+    //If message is for this device, or broadcast, print details:
+    Serial.println();
+    Serial.println("*********************************");
+    Serial.println("Rc from: 0x" + String(sender, HEX));
+    Serial.println("Message: " + incomingMessage);
+
+    ProcessingIncomingData();
+  }
+
+  void ProcessingIncomingData() {
+    //Check if sender addr is valid
+    bool isSenderAddrValid = false;
+    for (int i; i < 2; i++) {
+      if (slaveAddr == loraAddr[i]) {
+        isSenderAddrValid = true;
+        break;
+      }
+    }
+    if (!isSenderAddrValid) return;
+
+    //Getting loraIndex from sender address
+     int loraIndex = 0;
+    if(slaveAddr == loraAddr[0])
+    {
+      loraIndex = 0;
+    }
+    else if(slaveAddr == loraAddr[1])
+    {
+      loraIndex = 1;
+    }
+    else 
+    {
+      Serial.println("Dia chi khong hop le");
+      return;
+    }
+   
+    String isLedOnString = GetValue(incomingMessage, 0);
+    String batteryLevel =  GetValue(incomingMessage, 1);
+    batteryLevels[loraIndex] = batteryLevel.toInt();
+    Serial.println("Battery: " + String(batteryLevels[loraIndex]));
+
+    bool isLedData = false;
+    bool isBatteryData = false;
+
+    if (isLedOnString == "1" || isLedOnString == "0") {
+      isLedOn[loraIndex] = isLedOnString.toInt();
+      if (isLedOn[loraIndex] != lastLedState[loraIndex]) {
+        lastLedState[loraIndex] = isLedOn[loraIndex];
+
+        Serial.println("ESP32 Data Receive: " + String(isLedOn[loraIndex]));
+        isLedData = true;
+      }
+    }
+
+    int battery = batteryLevel.toInt();
+    if(battery != lastBatteryLevels[loraIndex])
+    {
+      lastBatteryLevels[loraIndex] = battery;
+      isBatteryData = true;
+    }
+    WriteFirebaseData(loraIndex, isLedData, isBatteryData);
+  }
+
+  //Code này dùng để tách khi gửi có nhiều dữ liệu cần tách thành chuỗi, Đối với ESP32 nhận bool isLedOn thì không cần
+  //Đối với Arduino nhận 2 bool isSensorOn, isLedOn thì cần tách một string thành những substring
+  //Data sẽ là string dữ liệu (parameter: String data) cách nhau bởi dấu " , "
+  //Hàm sẽ tìm index đầu cuối của string dữ liệu với vị trí xếp trong chuỗi cho trước (paramater: int index)
+  //Sau đó sẽ dùng hàm substring để lấy string dữ liệu cần dùng ra
+  String GetValue(String data, int index) {
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+      if (data.charAt(i) == separator || i == maxIndex) {
+        found++;
+        strIndex[0] = strIndex[1] + 1;
+        strIndex[1] = (i == maxIndex) ? i + 1 : i;
+      }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+  }
+
+  //Lora hoạt động thời gian dài có lúc bị đơ
+  //Workaround -> Reset lora theo định kỳ
+  void LoraResetHandler() {
+    unsigned long currentMillisRestartLORA = millis();
+    if (currentMillisRestartLORA - previousMillisRestartLORA >= intervalRestartLORA) {
+      previousMillisRestartLORA = currentMillisRestartLORA;
+      LoRa.end();
+      ResetLora();
+
+    }
+  }
+
+  void ResetLora() {
+    LoRa.setPins(ss, rst, dio0);
+
+    //Serial.println();
+    //Serial.println("Restart LoRa...");
+    //Serial.println("Start LoRa init...");
+    while (!LoRa.begin(433E6)) {
+      Serial.println("Starting LoRa failed!");
+      delay(100);
+    }
+    // Serial.println("LoRa init succeeded.");
+
+    // Reset the value of the Count_to_Rst_LORA variable.
+    resetLoraCount = 0;
+  }
+
+  //****************************END LORA HANDLER*****************************
